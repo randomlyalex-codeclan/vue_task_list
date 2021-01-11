8241cea759aab8c9c19779e7635cae5133dcb23b
Alex Dodd <alexdodd@gmail.com>
2021-01-11 23:31:42 +0000

    task list added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     el: "#app",
     data: {
       //data goes here
+      taskList: ["Wash Car", "Do Homework", "Walk Dog"],
     },
     methods: {
       //methods go here
