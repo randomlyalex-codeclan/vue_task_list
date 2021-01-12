fc06d6412e90cf716ce8973aed29f116db9b9d7c
Alex Dodd <alexdodd@gmail.com>
2021-01-12 01:55:37 +0000

    added priority

--- src/app.js
+++ src/app.js
@@ -6,16 +6,19 @@ document.addEventListener("DOMContentLoaded", () => {
     data: {
       //data goes here
       taskList: [
-          "Wash Car", "Do Homework", "Walk Dog"
+        { description: "Wash Car", priority: 1},
+        { description: "Do Homework", priority: 2},
+        { description: "Walk Dog", priority: 3}
         ],
-      newTask: ""
+      description: "",
+      priority: 5
     },
 
     methods: {
       //methods go here
       addNewTask: function(){
-        this.taskList.push(this.newTask);
-        this.newTask = "";
+        this.taskList.push({description: this.description, priority: this.priority});
+        this.description = "";
       },
       clearTasks: function(){
           this.taskList = []
